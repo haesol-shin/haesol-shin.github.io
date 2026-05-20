3048d962705a472c745385e0921f4b42cc413ada
haesol-shin <shinhaesol11@gmail.com>
2026-05-20 08:52:35 +0000

    Deploying to gh-pages from @ haesol-shin/haesol-shin.github.io@16060553120c14afa465567291878aa1eb50629a 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "haesol1013/haesol1013.github.io",
+    "data-repo": "haesol-shin/haesol-shin.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
